ad14880e4eb5dd2a987fab8b5445bff233bcc93b
badhon1 <ndcoc150@gmail.com>
2022-04-07 12:47:31 +0600

    new updata

---
+++ javascript.js
@@ -0,0 +1,6 @@
+const loginBtn = document.getElementById('login-btn').addEventListener('click',function(){
+    const loginArea = document.getElementById('login-area');
+    loginArea.style.display ='none';
+    let  tansactionArea = document.getElementById('tansaction-area');
+    tansactionArea.style.display = 'block';
+})
